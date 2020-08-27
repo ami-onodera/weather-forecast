fd600376093dde4060d06c9aad4bc1503769c4ab
Ami Onodera <onodera.camila@gmail.com>
2020-08-27 17:06:06 +0200

    weather app

--- src/script.js
+++ src/script.js
@@ -1,43 +1,60 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  moscow: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+// Data dinâmica
 
-function temperature() {
-  let city = prompt("Enter a city");
-  city = city.toLowerCase().trim();
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let weekDay = days[now.getDay()];
+let hourNow = now.getHours();
 
-  if (weather[city] !== undefined) {
-    let temperature = weather[city].temp;
-    let celsius = Math.round(temperature);
-    let fahrenheit = Math.round((temperature * 9) / 5 + 32);
+let minutesNow = now.getMinutes();
 
-    alert(
-      `It is currently ${celsius}°C (${fahrenheit}°F) in ${city} with a humidity of ${weather[city].humidity}%`
-    );
-  } else {
-    alert(
-      `Sorry, we don't know the weather for ${city}, try going to https://www.google.com/search?q=weather+${city}`
-    );
-  }
+if (minutesNow < 10) {
+  minutesNow = "0" + minutesNow;
+} else {
+  minutesNow = minutesNow + "";
 }
 
-temperature();
+let showDate = document.querySelector("#current-date");
+showDate.innerHTML = `${weekDay} ${hourNow}:${minutesNow}`;
+
+// Display city that was searched
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-bar");
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = ` ${searchInput.value}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
+// Show Celsius or Fahrenheit temperatures
+
+let temperatureInCelsius = 18;
+
+function tempCelsius(event) {
+  event.preventDefault();
+  let showTempCelsius = document.querySelector("#temp-number");
+  showTempCelsius.innerHTML = temperatureInCelsius;
+}
+
+let tempInCelsius = document.querySelector("#celsius-link");
+tempInCelsius.addEventListener("click", tempCelsius);
+
+function tempFahrenheit(event) {
+  event.preventDefault();
+  let temperatureInFahrenheit = (temperatureInCelsius * 9) / 5 + 32;
+  let showTempFahrenheit = document.querySelector("#temp-number");
+  showTempFahrenheit.innerHTML = temperatureInFahrenheit;
+}
+
+let tempInFahrenheit = document.querySelector("#fahrenheit-link");
+tempInFahrenheit.addEventListener("click", tempFahrenheit);
